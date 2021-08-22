d7a264cc4c65fc27606ab7ae72f80180f93a1e2e
fnixdev <fenixdevstar@hotmail.com>
2021-08-22 00:59:51 +0000

    @fnixdev

--- assistant/start.py
+++ assistant/start.py
@@ -23,7 +23,7 @@ Owner_info_msg = f"""
 
 <strong>Message Forwards</strong> - {udB.get("PMBOT")}
 
-<stong>Ultroid <a href=https://github.com/TeamUltroid/Ultroid>[v{ultroid_version}]</a>, powered by @fnixdev</strong>
+<stong>Kanna <a href=https://github.com/fnixdev/KannaBot>[v{ultroid_version}]</a>, powered by @fnixdev</strong>
 """
 
 _settings = [
